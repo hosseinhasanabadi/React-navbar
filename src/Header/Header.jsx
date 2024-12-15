93ec9d557ebbe88f473b5f2f16091e7f0e040d58
Hossein <hhasnabadi@yahoo.com>
2024-12-15 22:05:17 +0330

    navbar

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+import NavBar from './NavBar';
+export default function Header(){
+    return (
+       <div>
+         <section>
+            <span className='span'>
+                Hellow User
+            </span>
+          
+        </section>
+        <NavBar></NavBar>
+       </div>
+    )
+}
---
+++ src/Header/NavBar.jsx
@@ -0,0 +1,18 @@
+import './NavBar.css'
+function NavBar() {
+    return(
+        <header>
+        <h1>logo</h1>
+       <nav>
+         <ul>
+            <li>
+            <a href="">About</a>
+            <a href="">contant</a>
+            <a href="">servise</a>
+            </li>
+        </ul>
+       </nav>
+    </header>
+    )
+}
+export default NavBar
